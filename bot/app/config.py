f13f72f7466821757cd2746d9736f9e9782a5cc2
1110rank <romaruhman1806@gmail.com>
2026-08-13 00:41:23 +0500

    Update config.py

--- bot/app/config.py
+++ bot/app/config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     BOT_TOKEN: str
     API_BASE_URL: str = "http://localhost:8000"
 
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore", env_file_encoding="utf-8")
 
 
 settings = Settings()
